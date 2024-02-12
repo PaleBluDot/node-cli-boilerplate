71eac832da6d3fdd34910d74e6ccc12ce9aec6e6
Pavel Sanchez <23495830+PaleBluDot@users.noreply.github.com>
2024-02-12 14:00:56 -0500

    refactor(lib): add the description and vesion

--- src/commands/flags/usage.js
+++ src/commands/flags/usage.js
@@ -1,27 +1,30 @@
 const chalk = require('chalk');
+const pkg = require('../../../package.json');
 
 module.exports = function usage() {
 	console.log(`
-${chalk.whiteBright('tool [CMD]')}
+${chalk.whiteBright(`${pkg.name} [CMD]`)} v${pkg.version}
+
+${pkg.description}
 
 Commands:
-  ${chalk.greenBright('--start, -s')}
+  ${chalk.greenBright('start')}
     Starts the app. This will initialize the application and start running it.
 
-  ${chalk.greenBright('--build, -b')}
+  ${chalk.greenBright('build')}
     Builds the app. This will compile the application and prepare it for deployment.
 
-  ${chalk.greenBright('--help, -h')}
+  ${chalk.greenBright('help')}
     Displays this help message. Use this command if you need information about how to use the tool.
 
 Examples:
-  ${chalk.cyanBright('tool --start')}
+  ${chalk.cyanBright(`${pkg.name} start`)}
     Start the application.
 
-  ${chalk.cyanBright('tool --build')}
+  ${chalk.cyanBright(`${pkg.name} build`)}
     Build the application.
 
-  ${chalk.cyanBright('tool --help')}
+  ${chalk.cyanBright(`${pkg.name} help`)}
     Display the help message.
 
 For more information, visit ${chalk.underline.blueBright(
